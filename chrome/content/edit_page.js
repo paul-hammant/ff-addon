507bbb9286b97a33a4cd7912a8f9d77b7dae66cd
Paul Hammant <paul@hammant.org>
2017-12-28 12:04:57 -0500

    start

---
+++ chrome/content/edit_page.js
@@ -0,0 +1,100 @@
+/* oncommand event */
+function edit_page() {
+
+    var url = getWebNavigation().currentURI.spec;
+
+    if (url.substr(0, 4) == "http") {
+        if (url.indexOf("?") > 0) {
+            url = url + "&editMode=true";
+        } else {
+            url = url + "?editMode=true";
+        }
+    }
+
+
+    // This API is not allowed in WebExtensions (Firefox 57 API)
+    var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+    var args;
+    var found = false;
+
+    // Mac OS X
+
+    try {
+        file.initWithPath("/Applications/KompoZer.app/Contents/MacOS/kompozer-bin");
+        if (file.exists()) {
+            found = true;
+            args = new Array(url);
+        }
+    } catch (ex) {
+    }
+
+
+    if (!found) {
+        try {
+            file.initWithPath("/Applications/SeaMonkey.app/Contents/MacOS/seamonkey");
+            if (file.exists()) {
+                found = true;
+                args = new Array("-edit", url);
+            }
+        } catch (ex) {
+        }
+    }
+
+    // Windows
+    if (!found) {
+        try {
+            file.initWithPath("C:\\Program Files\\KompoZer\\kompozer.exe");
+            if (file.exists()) {
+                found = true;
+                args = new Array(url);
+            }
+        } catch (ex) {
+        }
+    }
+
+    if (!found) {
+        try {
+            file.initWithPath("C:\\Program Files\\mozilla.org\\SeaMonkey\\seamonkey.exe");
+            if (file.exists()) {
+                found = true;
+                args = new Array("-editor", url);
+            }
+        } catch (ex) {
+        }
+    }
+
+    // Linux
+
+    if (!found) {
+         try {
+             file.initWithPath("/usr/local/kompozer/kompozer");
+             if (file.exists()) {
+                 found = true;
+                 args = new Array(url);
+             }
+         } catch (ex) {
+         }
+     }
+
+    if (!found) {
+         try {
+             file.initWithPath("/usr/local/seamonkey/seamonkey");
+             if (file.exists()) {
+                 found = true;
+                 args = new Array("-editor", url);
+             }
+         } catch (ex) {
+         }
+     }
+
+
+    if (found) {
+        // This API is not allowed in WebExtensions (Firefox 57 API)
+        var process = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+        process.init(file);
+        process.run(false, args, args.length);
+    } else {
+        window.alert("No editor found. Please install SeaMonkey!");
+    }
+
+}
